bf52babb98c0f34b250ed4ff5001cbee42160517
Ben Coons <coonsb@purdue.edu>
2015-11-14 05:43:31 -0500

    added new types of grids

--- js/sketchgrid.js
+++ js/sketchgrid.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-	createGrid(16);
+	createGrid(16, 1);
 
-	function createGrid(width) {
+	function createGrid(width, type) {
 		var tileDim = (750 -(2 * width)) / width;
 		for (var i = 0; i < (width * width); i++) {
 			$('.container').append('<div class="tile"></div>');
@@ -9,15 +9,17 @@ $(document).ready(function() {
 		$('.tile').height(tileDim);
 		$('.tile').width(tileDim);
 		$('.tile').mouseenter(function() {
-		$(this).css("background-color", "#000");
-	});
-	}
+			if (type === 1) {
+				$(this).css("background-color", "#000");
+			}
+		});
+	};
 
 	$('#clear').click(function() {
 		$('.container').empty();
 		var newWidth = prompt("How wide do you want the new grid to be? (Greater than 0 and less than or equal to 128)");
 		if (newWidth > 0 && newWidth <= 128) {
-			createGrid(newWidth);
+			createGrid(newWidth, 1);
 		} else {
 			alert("Please enter a valid grid width!");
 		}
