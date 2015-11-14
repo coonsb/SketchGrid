0307a4409949048f6ba444c57bf03d19979bb7cd
Ben Coons <coonsb@purdue.edu>
2015-11-14 04:39:34 -0500

    Added reset file

--- js/sketchgrid.js
+++ js/sketchgrid.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-	for (var i = 0; i < 64; i++) {
-		$('#container').append('<div class="tile" id="' + i + '"></div>');
+	var tileDim = (500 - 64) / 16;
+	for (var i = 0; i < 256; i++) {
+		$('.container').append('<div class="tile"></div>');
 	}
+	$('.tile').css('height', tileDim);
+	$('.tile').css('width', tileDim);
 });	
