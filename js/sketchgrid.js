4d8aeece4ced2a3d523d58a0e906262291f776ba
Ben Coons <coonsb@purdue.edu>
2015-11-14 05:38:47 -0500

    Fixed margin between blocks

--- js/sketchgrid.js
+++ js/sketchgrid.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	createGrid(16);
 
 	function createGrid(width) {
-		var tileDim = (750 -(3 * width)) / width;
+		var tileDim = (750 -(2 * width)) / width;
 		for (var i = 0; i < (width * width); i++) {
 			$('.container').append('<div class="tile"></div>');
 		}
@@ -15,7 +15,11 @@ $(document).ready(function() {
 
 	$('#clear').click(function() {
 		$('.container').empty();
-		var newWidth = prompt("How wide do you want the new grid to be?");
-		createGrid(newWidth);
+		var newWidth = prompt("How wide do you want the new grid to be? (Greater than 0 and less than or equal to 128)");
+		if (newWidth > 0 && newWidth <= 128) {
+			createGrid(newWidth);
+		} else {
+			alert("Please enter a valid grid width!");
+		}
 	});
 });
