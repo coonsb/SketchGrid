7e8e0b785f5353b21ef3b213f0a937520929e45d
Ben Coons <coonsb@purdue.edu>
2015-11-14 04:59:56 -0500

    Created grid creation function

--- js/sketchgrid.js
+++ js/sketchgrid.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-	var tileDim = (750 - 48) / 16;
-	for (var i = 0; i < 256; i++) {
-		$('.container').append('<div class="tile"></div>');
-	}
-	$('.tile').height(tileDim);
-	$('.tile').width(tileDim);
+	createGrid(16);
 
 	$('.tile').hover(function() {
 		$(this).css("background-color", "#000");
 	});
 
 });	
+
+function createGrid(width) {
+	var tileDim = (750 -(3 * width)) / width;
+	for (var i = 0; i < (width * width); i++) {
+		$('.container').append('<div class="tile"></div>');
+	}
+	$('.tile').height(tileDim);
+	$('.tile').width(tileDim);
+}
