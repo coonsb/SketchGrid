7a2a956d08ac4e584f5afec6af7ff7d15a154c95
Ben Coons <coonsb@purdue.edu>
2015-11-14 04:50:32 -0500

    Tiles change color when hovered over

--- js/sketchgrid.js
+++ js/sketchgrid.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
-	var tileDim = (500 - 64) / 16;
+	var tileDim = (750 - 48) / 16;
 	for (var i = 0; i < 256; i++) {
 		$('.container').append('<div class="tile"></div>');
 	}
-	$('.tile').css('height', tileDim);
-	$('.tile').css('width', tileDim);
+	$('.tile').height(tileDim);
+	$('.tile').width(tileDim);
+
+	$('.tile').hover(function() {
+		$(this).css("background-color", "#000");
+	});
+
 });	
